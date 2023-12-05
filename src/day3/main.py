89e34762598ef8a2130d15e0fd8e227602604934
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2023-12-05 20:27:39 +0100

    Add day 3

---
+++ src/day3/main.py
@@ -0,0 +1,44 @@
+import re
+import sys
+from pathlib import Path
+from pprint import pp
+
+################################################################################
+
+
+def part1(data):
+    return None
+
+
+################################################################################
+
+
+def part2(data):
+    return None
+
+
+################################################################################
+
+
+def parse(path):
+    data = Path(path).read_text().splitlines()
+
+    # data = [(x, int(y)) for x, y in data]
+    # data = [[[int(x) for x in y] for y in line] for line in data]
+    # data = [[int(x) for x in line] for line in data]
+    # data = [[x for x in line] for line in data]
+    # data = [[x.split(",") for x in line] for line in data]
+    # data = [line.split(" ") for line in data]
+    # data = [line.split(",") for line in data]
+    # data = [line.strip("\n") for line in data]
+    # data = [x.split(" -> ") for x in data]
+    # data = [x[0] for x in data]
+
+    return data
+
+
+# data = "input.txt"
+data = "example.txt"
+
+print(part1(parse(data)))
+print(part2(parse(data)))
